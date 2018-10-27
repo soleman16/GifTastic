b489fe92a4f9c629d47fd358143da6675a191ab9
Darren Anastasoff <soleman16@gmail.com>
2018-10-27 09:26:01 -0400

    completed project with bonus functionality included

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # GifTastic
+
+## Before You Begin
+
+1. Hit the GIPHY API.
+
+    1. Fool around with the GIPHY API. Giphy API.
+
+    1. Be sure to read about these GIPHY parameters (hint, hint):
+        1. q
+        1. limit
+        1. rating
+
+    1. Like many APIs, GIPHY requires developers to use a key to access their API data. To use the GIPHY API, you'll need a GIPHY account (don't worry, it's free!) and then obtain an API Key by creating an app.
+
+    1. Make sure you switch the protocol in the query URL from http to https, or the app may not work properly when deployed to Github Pages.
+
+1. Watch the demo video
+
+1. You should have a high-level understanding of how this assignment works before attempting to code it.
+
+## Submission on BCS
+
+1. Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
+
+## Instructions
+
+1. Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+
+    1. We chose animals for our theme, but you can make a list to your own liking.
+
+1. Your app should take the topics in this array and create buttons in your HTML.
+
+    1. Try using a loop that appends a button for each string in the array.
+
+1. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+1. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+1. Under every gif, display its rating (PG, G, so on).
+
+    1. This data is provided by the GIPHY API.
+
+    1. Only once you get images displaying with button presses should you move on to the next step.
+
+1. Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+
+1. Deploy your assignment to Github Pages.
+
+1. Rejoice! You just made something really cool.
+
+## Bonus Goals
+
+1. Ensure your app is fully mobile responsive.
+
+1. Allow users to request additional gifs to be added to the page.
+
+    1. Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
+
+1. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
+
+1. Include a 1-click download button for each gif, this should work across device types.
+
+1. Integrate this search with additional APIs such as OMDB, or Bands in Town. Be creative and build something you are proud to showcase in your portfolio
+
+1. Allow users to add their favorite gifs to a favorites section.
+
+    1. This should persist even when they select or add a new topic.
+
+    1. If you are looking for a major challenge, look into making this section persist even when the page is reloaded (via localStorage or cookies).
+
+## Reminder: Submission on BCS
+
+Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
---
+++ assets/javascript/app.js
@@ -0,0 +1,403 @@
+let gifManager = {
+    selectedGifs: [{}],
+    favoriteGifs: [{}],
+    gifLimit: 10,
+    getGifs: function (userSearch, limit){
+        let url = "https://api.giphy.com/v1/gifs/search?q=";
+        let apiKey = "&api_key=VrVNHn3sUOMfhqftGc4ZMMu8vcTrbadC"; 
+        let limitParameter = "&limit=" + limit;
+
+        $.ajax({
+            url: url + userSearch + " football" + apiKey + limitParameter,
+            success: function(result) {
+                console.log(this.url);
+                gifManager.clearGifs();
+                for(var index in result.data){
+                    let currentGif = result.data[index];
+                    let title = currentGif.title;
+                    let rating = currentGif.rating;
+                    let stillUrl = currentGif.images["fixed_height_still"].url;
+                    let animationUrl = currentGif.images["fixed_height"].url;
+                    gifManager.createGifCard(rating, animationUrl, stillUrl, title, index);
+                }
+            }
+        });
+    },
+    clearSelectedGifs: function(){
+        gifManager.selectedGifs = [{}];
+    },
+    clearGifs: function(){
+        $("#gif-column").empty();
+    },
+    clearFavorites: function(){
+        $(".favorites").empty();
+    },
+    download: function(url, fileName){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = "blob";
+        xhr.onload = function(){
+            var urlCreator = window.URL || window.webkitURL;
+            var imageUrl = urlCreator.createObjectURL(this.response);
+            var tag = document.createElement('a');
+            tag.href = imageUrl;
+            tag.download = fileName;
+            document.body.appendChild(tag);
+            tag.click();
+            document.body.removeChild(tag);
+        }
+        xhr.send();
+    },
+    doesGifExist: function(title){
+
+        for (let index in gifManager.selectedGifs){
+            let currentGif = gifManager.selectedGifs[index];
+            if(currentGif.title && currentGif.title === title){
+                return true;
+            }
+        }
+    
+        return false;
+    },
+    clearDropDown: function(){
+        $(".dropdown-menu").empty();
+    },  
+    displayFavorites: function(){
+
+        console.log("display favorites");
+    
+        gifManager.clearDropDown();
+    
+        let dropDownElement = $(".dropdown-menu");
+    
+        for(let index in gifManager.favoriteGifs){
+            let currentUrl = gifManager.favoriteGifs[index].url;
+            let currentTitle = gifManager.favoriteGifs[index].title;
+    
+            let menuItem = $("<a>", {
+                class: "dropdown-item",
+                text: currentTitle, 
+                href: currentUrl
+            })
+    
+            dropDownElement.append(menuItem);
+        }
+    },
+    createGifCard: function(rating, animationUrl, stillUrl, title){
+
+        let gifColumnDiv = $("#gif-column");
+    
+        let card = $("<div>" , {
+            class: "card border-secondary mb-3 mr-5 d-inline-block",
+            style: "max-width: 200px; height: 300px",
+        });
+        let cardHeader = $("<div>" , {
+            class: "card-header text-center",
+            text: "Rating: " + rating.toUpperCase()
+        });
+        let cardBody = $("<div>" , {
+            class: "card-body text-secondary flex-column d-flex h-25"
+        });
+        let cardTitle = $("<h5>" , {
+            class: "card-title",
+            text: title,
+            style: "font-size: 12px;"
+        });
+        let cardImage = $("<img>", {
+            class: "img-responsive team-image",
+            src: stillUrl,
+            style: "width: 198px; height: 120px",
+            "data-animation-state": "still",
+            "data-animation-url": animationUrl,
+            "data-still-url": stillUrl,
+        })
+        let cardCheckbox = $('<input>', {
+            class: "card-checkbox",
+            type: 'checkbox',
+            "data-gif-url": animationUrl,
+            "data-gif-title": title
+        });
+    
+        let cardFooter = $("<div>", {
+            class: "card-footer bg-transparent border-secondary text-center py-3"
+        })
+    
+        cardBody.append(cardTitle)
+        cardFooter.append(cardCheckbox);
+        card.append(cardHeader, cardImage, cardBody, cardFooter);
+        gifColumnDiv.append(card);
+    }
+}
+
+let nfl = {
+    abbreviatedTeamName: "",
+    displayedTeams: [{}],
+    nflTeams: 
+        [{
+            team: "Arizona Cardinals",
+            image: "assets/images/cardinals.gif"
+        },
+        {
+            team: "Atlanta Falcons",
+            image: "assets/images/falcons.gif"
+        },
+        {
+            team: "Baltimore Ravens",
+            image: "assets/images/ravens.gif"
+        },
+        {
+            team: "Buffalo Bills",
+            image: "assets/images/bills.gif"
+        },
+        {
+            team: "Carolina Panthers", 
+            image: "assets/images/panthers.gif"
+        },
+        {
+            team: "Chicago Bears",
+            image: "assets/images/bears.gif"
+        },
+        {
+            team: "Cinncinati Bengals", 
+            image: "assets/images/bengals.gif"
+        },
+        {
+            team: "Cleveland Browns",
+            image: "assets/images/browns.gif"
+        },
+        {
+            team: "Dallas Cowboys",
+            image: "assets/images/cowboys.gif"
+        },
+        {
+            team: "Denver Broncos",
+            image: "assets/images/broncos.gif"
+        },
+        {
+            team: "Detroit Lions",
+            image: "assets/images/lions.gif"
+        },
+        {
+            team: "Green Bay Packers",
+            image: "assets/images/packers.gif"
+        }],
+    getTeams: function(){
+        $.ajax({
+            url: "https://api.fantasydata.net/v3/nfl/stats/JSON/TeamSeasonStats/2018",
+            headers: { 'Ocp-Apim-Subscription-Key': '0f4c5936b24a49d4b8e7b9f6e1c6c81c' },
+            success: function(result) {
+                for(let index in result){
+                    let currentTeam = result[index];
+                    nfl.displayedTeams.push(currentTeam);
+                }
+            }
+        });
+    },
+    addLogos: function(nflTeams){
+
+        for(let index in nflTeams){
+    
+            let image = nflTeams[index].image;
+            let team = nflTeams[index].team;
+    
+            let gifLogoSectionDiv = $("#gif-logo-section");
+    
+            let teamImage = $("<img>", {
+                class: "rounded m-5 d-block logo",
+                style: "height: 75px; width: 100px",
+                src: image,
+                "data-team-name": team
+            });
+    
+            gifLogoSectionDiv.append(teamImage);
+        }
+    },
+    getTeamInformation: function(teamName){
+        for(let index in nfl.displayedTeams){
+    
+            let currentTeam = nfl.displayedTeams[index];
+    
+            if (!jQuery.isEmptyObject(currentTeam)){
+    
+                let currentTeamName = currentTeam.TeamName.toLowerCase();
+        
+                if(teamName.toLowerCase().includes(currentTeamName)){
+                    return currentTeam;
+                }
+            }
+        }
+    },
+    getTeamNews: function(){
+        $.ajax({
+            url: "https://api.fantasydata.net/v3/nfl/stats/JSON/NewsByTeam/" + nfl.abbreviatedTeamName ,
+            headers: { 'Ocp-Apim-Subscription-Key': '0f4c5936b24a49d4b8e7b9f6e1c6c81c' },
+            success: function(result) {
+                nfl.displayNews(result);
+            }
+        });
+    },
+    displayTeamHeading: function(){
+        let teamHeadingRow = $("#team-heading");
+        
+        let teamHeadingElement = $("<h1>", {
+            class: "text-center",
+            text: nfl.abbreviatedTeamName
+        })
+    
+        teamHeadingRow.append(teamHeadingElement);
+    },
+    clearTeamHeading: function(){
+        $("#team-heading").empty();
+    },
+    clearLogos: function(){
+        $("#gif-logo-section").empty();
+    },
+    clearNews: function(){
+        $("#team-news-column").empty();
+    },
+    displayNews: function(news){
+        let teamNewsDiv = $("#team-news-column");
+    
+        for(let index in news){
+    
+            let currentNewsItem = news[index];
+    
+            let headerElement = $("<h5>", {
+                class: "text-muted",
+                text: currentNewsItem.Title
+            })
+        
+            let timeAgoElement = $("<p>", {
+                text: currentNewsItem.TimeAgo
+            })
+    
+            let contentElement = $("<p>", {
+                text: currentNewsItem.Content
+            })
+        
+            teamNewsDiv.append(headerElement, timeAgoElement, contentElement); 
+        }
+    }
+}
+
+/************************************* Start GifTastic ***************************************************/
+
+$(document).ready(function(){
+
+    // Retrieve the object from storage
+    let localDb = JSON.parse(localStorage.getItem('favorites'));
+
+    if(localDb){
+        gifManager.favoriteGifs = localDb;
+        gifManager.displayFavorites();
+    }
+
+    nfl.addLogos(nfl.nflTeams);
+
+    nfl.getTeams();
+
+    $("#search-button").on("click", function(){
+        event.preventDefault();
+
+        let userSearch = $("#search-text").val();
+
+        let team = nfl.getTeamInformation(userSearch);
+
+        let teamName = team.TeamName.toLowerCase();
+
+        let userEnteredTeam = {
+            team: userSearch,
+            image: "assets/images/" + teamName + ".gif"
+        }
+        nflTeams.push(userEnteredTeam);
+        clearGifs();
+        clearLogos();
+        clearNews();
+        clearTeamHeading();
+        addLogos(nflTeams);
+    });
+
+    $(document).on("click", "#download-gifs-button", function(){
+        for(let index in gifManager.selectedGifs){
+            let currentUrl = gifManager.selectedGifs[index].url;
+            gifManager.download(currentUrl, "teams.gif");
+        }
+
+        // reset to empty array because the favorite button looks at this same list
+        gifManager.clearSelectedGifs();
+        $(".card-checkbox").prop('checked', false);
+    });
+
+    $(document).on("click", "#clear-favorites", function(){
+        gifManager.clearFavorites();
+        localStorage.clear();
+    });
+
+    $(document).on("click", "#favorite-gifs-button", function(){
+        gifManager.favoriteGifs = gifManager.selectedGifs;
+        gifManager.clearSelectedGifs();
+        gifManager.displayFavorites();
+
+        $(".card-checkbox").prop('checked', false);
+       
+        // Put the object into storage
+        localStorage.setItem('favorites', JSON.stringify(gifManager.selectedGifs));
+    });
+
+    $(document).on("click", "#add-gifs-button", function(){
+        gifManager.getGifs(nfl.abbreviatedTeamName, gifManager.gifLimit + 10);
+    });
+
+    // user clicks one of the logos with the logos
+    $(document).on("click", ".logo", function(){
+        let userSearch = $(this).attr("data-team-name");
+        console.log(userSearch);
+        let team = nfl.getTeamInformation(userSearch);
+        nfl.abbreviatedTeamName = team.Team;
+        nfl.getTeamNews();
+        nfl.clearTeamHeading();
+        nfl.displayTeamHeading();
+        gifManager.clearGifs();
+        nfl.clearNews();
+        gifManager.getGifs(userSearch, gifManager.gifLimit);
+    });
+
+    $(document).on("click", ".card-checkbox", function(){
+        let userSelectedGifUrl = $(this).attr("data-gif-url");
+        let userSelectedGifTitle = $(this).attr("data-gif-title");
+
+        let selectedGif = {
+            title: userSelectedGifTitle, 
+            url: userSelectedGifUrl
+        }
+
+        if(!gifManager.doesGifExist()){
+            gifManager.selectedGifs.push(selectedGif);
+        } 
+    });
+
+    // user clicks one of the logos with the logos
+    $(document).on("click", ".home", function(){
+        nfl.clearTeamHeading();
+        gifManager.clearGifs();
+        nfl.clearNews();
+    });
+
+    // one of the gifs
+    $(document).on("click", ".team-image", function(){
+        let selectedImageState = $(this).attr("data-animation-state");
+
+        if(selectedImageState === "still"){
+            let animationUrl = $(this).attr("data-animation-url");
+            $(this).attr("src", animationUrl);
+            $(this).attr("data-animation-state", "animated");
+        }
+        else{
+            let stillUrl = $(this).attr("data-still-url");
+            $(this).attr("src", stillUrl);
+            $(this).attr("data-animation-state", "still");
+        }
+    });
+});
+
+/************************************* Start GifTastic ***************************************************/
